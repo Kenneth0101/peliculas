de63cf1818ff13a3830abd7e255975db16e9df6a
Kenneth0101 <84344796+Kenneth0101@users.noreply.github.com>
2022-08-17 10:42:41 -0500

    first commit

---
+++ view/actores-peliculas/delete.php
@@ -0,0 +1,20 @@
+<?php include 'header.php' ?>
+<!-- Main content -->
+<section class="content"> <!-- /.aqui empieza el dashboard para la programacion ------------------------------------------------------------->
+<h3>Nuevo Actor Peliculas</h3>       
+<table style="margin: 0 auto; border: black 3px solid;">
+        <tr>
+            <td>Nombre:</td>
+            <td>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</td>
+        </tr>
+        <tr>
+            <td>Direccion:</td>
+            <td>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</td>
+        </tr>
+    </table>
+    <br>
+
+    <button style="margin: 0 auto; background-color: red;" >Delete</button>
+</section>
+
+<?php include 'footer.php' ?>
---
+++ view/index.php
@@ -0,0 +1,7 @@
+<?php include 'template/header.php' ?>
+<!-- Main content -->
+<section class="content"> <!-- /.aqui empieza el dashboard para la programacion ------------------------------------------------------------->
+    Hi word
+</section>
+
+<?php include 'template/footer.php' ?>
